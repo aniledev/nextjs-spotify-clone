19906456edeb11e720c93353144e7c6679975c4c
Elina McGill <elinamcgill@gmail.com>
2023-10-11 12:04:53 -0400

    Define routes for application

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,17 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/**
+ * @todo Remove no unsafe assignment disable with correct typing
+ * @body HiHome and BiSearch elements are typed correctly but still triggering the rule. Figure out why and fix.
+ */
 "use client";
 
-import React from 'react'
+import React, { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { type IconType } from "react-icons";
 
-interface SideBarProps {
-  children: React.ReactNode;
-}
+type SidebarProps = {
+	children: React.ReactNode;
+};
 
-export const Sidebar: React.FC<SideBarProps> = ({
-  children
-}) => {
-  return (
-    <div>{children}</div>
-  )
-}
+type Route = {
+	active: boolean;
+	href: string;
+	icon: IconType;
+	label: string;
+};
 
-export default Sidebar;
+export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+	const pathname = usePathname();
+	const isPathHome = pathname === "/";
+
+	const routes: Route[] = useMemo(() => [
+		{
+			active: isPathHome,
+			href: "/",
+			icon: HiHome,
+			label: "Home",
+		},
+		{
+			active: !isPathHome,
+			href: "/search",
+			icon: BiSearch,
+			label: "Search",
+		}
+	], [pathname]);
+
+	return (
+		<div className='flex h-full'>
+			<div className='hidden'></div>
+		</div>
+	);
+};

03833a4452129ed4620382efdd4d9d9980347309
Elina McGill <elinamcgill@gmail.com>
2023-10-12 22:07:03 -0400

    Flesh out navigational links

--- app/components/SidebarItem.tsx
+++ app/components/SidebarItem.tsx
@@ -1,14 +1,38 @@
 import React from "react";
+import Link from "next/link";
+import { type IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
 
 type SidebarItemProps = {
-	children: React.ReactNode;
-	className?: string;
+	active?: boolean;
+	href: string;
+	icon: IconType;
+	label: string;
 };
 
 export const SidebarItem: React.FC<SidebarItemProps> = ({
-	children,
-	className
+	active,
+	href,
+	icon,
+	label
 }) => (
-	<div>SidebarItem</div>
+	<Link
+		href={href}
+		className={twMerge(`
+      flex
+      flex-row
+      h-auto
+      w-full
+      items-center
+      gap-x-4
+      text-md
+      font-medium
+      cursor-pointer
+      hover:text:white
+      transition
+      text-neutral-400
+      py-1
+    `, active && "text-white")}
+	>SidebarItem</Link>
 );
 

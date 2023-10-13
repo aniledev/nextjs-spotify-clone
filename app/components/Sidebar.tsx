2b05dcd011cd627ac0163e7973c5617e41ab78cb
Elina McGill <elinamcgill@gmail.com>
2023-10-12 21:57:59 -0400

    Create bare bones Sidebar component

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -10,6 +10,9 @@ import { usePathname } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { type IconType } from "react-icons";
+// < ---- COMPONENTS ---- >
+import { Box } from "./Box";
+import { SidebarItem } from "./SidebarItem";
 
 type SidebarProps = {
 	children: React.ReactNode;
@@ -43,7 +46,38 @@ export const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
 	return (
 		<div className="flex h-full">
-			<div className=""></div>
+			<div className="
+        hidden
+        md:flex
+        flex-col
+        gap-y-2
+        bg-black
+        h-full
+        w-[300px]
+        p-2
+      "
+			>
+
+				<Box>
+					<div className="
+            flex
+            flex-col
+            gap-y-4
+            px-5
+            py-4
+          ">
+						{routes.map(item => (
+							<SidebarItem
+								key={item.label}
+								{...item}
+							/>
+						))}
+					</div>
+				</Box>
+				<Box className="overflow-y-auto h-full">
+          Song Library
+				</Box>
+			</div>
 		</div>
 	);
 };

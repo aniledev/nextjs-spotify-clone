73f9e5b0b9a0efbd63c6eb668b866c024782e8ec
Elina McGill <elinamcgill@gmail.com>
2023-10-15 14:44:06 -0400

    Flesh out sign up and log in button components

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -55,13 +55,32 @@ export const Header: React.FC<HeaderProps> = ({
 				<div className="flex justify-between items-center gap-x-4">
 					<>
 						<div>
-							<Button>
+							<Button
+								className="
+                  bg-transparent
+                  text-neutral-300
+                  font-medium
+                "
+							>
                 Sign Up
+							</Button>
+						</div>
+						<div>
+							<Button
+								// OnClick={() => {}}
+								className="
+                  bg-white
+                  px-6
+                  py-2
+                "
+							>
+                Log in
 							</Button>
 						</div>
 					</>
 				</div>
 			</div>
+			{children}
 		</div>
 	);
 };

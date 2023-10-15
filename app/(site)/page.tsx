72016f37bf4e6f56a7cee5ae79f8cf71f29f1d3d
Elina McGill <elinamcgill@gmail.com>
2023-10-15 14:57:51 -0400

    Create bare bones list item component for rendering individual song items

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { Header } from "../components/Header";
+import { ListItem } from "../components/ListItem";
 
 export default function Home() {
 	return (
 		<div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
 			<Header>
-        Header
+				<div className="mb-2">
+					<h1 className="text-white font-3xl font-semibold">
+            Welcome Back
+					</h1>
+					<div>
+						<ListItem image={""} name={""} href={""}/>
+					</div>
+				</div>
 			</Header>
 		</div>
 	);

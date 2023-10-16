4c1da4a4697f917fdfcf35c87c03d12d2a3b8aea
Elina McGill <elinamcgill@gmail.com>
2023-10-15 20:56:20 -0400

    Give list item styling and transition for play icon

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
 		<div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
 			<Header>
 				<div className="mb-2">
-					<h1 className="text-white font-3xl font-semibold">
+					<h1 className="text-white text-3xl font-semibold">
             Welcome Back
 					</h1>
-					<div>
-						<ListItem image={""} name={""} href={""}/>
+					<div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+						<ListItem image="/images/liked.png" name="Liked Songs" href="liked"/>
 					</div>
 				</div>
 			</Header>
--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/navigation";
-import React from "react";
+// <------- COMPONENTS ---------->
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 
 type ListItemProps = {
 	image: string;
@@ -8,9 +10,9 @@ type ListItemProps = {
 };
 
 export const ListItem: React.FC<ListItemProps> = ({
+	href,
 	image,
-	name,
-	href
+	name
 }) => {
 	const router = useRouter();
 
@@ -20,7 +22,31 @@ export const ListItem: React.FC<ListItemProps> = ({
 	};
 
 	return (
-		<div>ListItem</div>
+		<button className="
+        relative
+        group
+        flex
+        items-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-100/10
+        cursor-pointer
+        hover:bg-neutral-100/20
+        transition
+        pr-4
+        "
+		>
+			<div className="relative min-h-[64px] min-w-[64px]">
+				<Image fill className="object-cover" src={image} alt="White heart on purple gradient background"/>
+			</div>
+			<p className="font-medium truncate py-5">{name}</p>
+			<div
+				className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110"
+			>
+				<FaPlay className="text-black"/>
+			</div>
+		</button>
 	);
 };
 

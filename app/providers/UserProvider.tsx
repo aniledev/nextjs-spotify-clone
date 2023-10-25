d888327eaf053e6db3296099c3d9a63bae0f25d5
Elina McGill <elinamcgill@gmail.com>
2023-10-25 17:09:55 -0400

    Wire up user provider for supabase

---
+++ app/providers/UserProvider.tsx
@@ -0,0 +1,14 @@
+import { UserContextProvider } from "../hooks/useUser";
+
+type UserProviderProps = {
+	children: React.ReactNode;
+};
+
+export const UserProvider: React.FC<UserProviderProps> = ({
+	children
+}) => (
+	<UserContextProvider>
+		{children}
+	</UserContextProvider>
+);
+

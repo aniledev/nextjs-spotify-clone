f45050d305cce92b01b990e7b2907428962c7963
Elina McGill <elinamcgill@gmail.com>
2023-10-25 12:28:28 -0400

    Fix useless template literal

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -1,12 +1,12 @@
 import { twMerge } from "tailwind-merge";
 
 type BoxProps = {
-  children: React.ReactNode;
-  className?: string;
+	children: React.ReactNode;
+	className?: string;
 };
 
 export const Box: React.FC<BoxProps> = ({ children, className }) => (
-  <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>
-    {children}
-  </div>
+	<div className={twMerge("bg-neutral-900 rounded-lg h-fit w-full", className)}>
+		{children}
+	</div>
 );
